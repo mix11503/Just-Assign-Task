491245d268eb907d366d5b0816363eeb379e86d4
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-03-30 16:25:52 +0700

    เพิ่ม class Teacher

---
+++ src/java/com/junior/jat/model/Teacher.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.junior.jat.model;
+
+/**
+ *
+ * @author PNattawut
+ */
+public class Teacher {
+    private int teacherId;
+    private String password;
+    private String name;
+
+    public Teacher() {
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" + "teacherId=" + teacherId + ", password=" + password + ", name=" + name + '}';
+    }
+    
+    
+}
