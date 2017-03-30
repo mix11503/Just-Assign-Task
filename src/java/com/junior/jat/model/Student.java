1d5218f6eee437a8c95b21750aaad5d5b9821fb0
baskittisak <kittisak.bas@mail.kmutt.ac.th>
2017-03-30 16:16:52 +0700

    Add Student.java and Subject.java

---
+++ src/java/com/junior/jat/model/Student.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.junior.jat.model;
+
+/**
+ *
+ * @author Kittisak
+ */
+public class Student {
+    private String name;
+    private String password;
+    private long studentId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", password=" + password + ", studentId=" + studentId + '}';
+    }
+    
+    
+}
+
+
---
+++ src/java/com/junior/jat/model/Subject.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.junior.jat.model;
+
+/**
+ *
+ * @author Kittisak
+ */
+public class Subject {
+    private String subjectId;
+    private String subjectName;
+    private long teacherId;
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subject{" + "subjectId=" + subjectId + ", subjectName=" + subjectName + ", teacherId=" + teacherId + '}';
+    }
+    
+    
+}
