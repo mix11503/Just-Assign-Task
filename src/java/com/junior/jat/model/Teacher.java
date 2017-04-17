90b05bce2177bdfa71a59ebed898475477bfc7ba
MayPT <pattamawan.Thongjam@mail.kmutt.ac.th>
2017-04-17 22:43:26 +0700

    สร้าง login.jsp แก้ไข  LoginServlet แก้ไข model Student แก้ไข model Teacher

--- src/java/com/junior/jat/model/Teacher.java
+++ src/java/com/junior/jat/model/Teacher.java
@@ -5,23 +5,27 @@
  */
 package com.junior.jat.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author PNattawut
  */
 public class Teacher {
-    private int teacherId;
+    private long teacherId;
     private String password;
     private String name;
 
     public Teacher() {
     }
 
-    public int getTeacherId() {
+    public long getTeacherId() {
         return teacherId;
     }
 
-    public void setTeacherId(int teacherId) {
+    public void setTeacherId(long teacherId) {
         this.teacherId = teacherId;
     }
 
@@ -45,6 +49,26 @@ public class Teacher {
     public String toString() {
         return "Teacher{" + "teacherId=" + teacherId + ", password=" + password + ", name=" + name + '}';
     }
-    
+     public static Teacher login(long id, String password) {
+        Teacher t = new Teacher();
+        
+        try {
+            Connection conn = BuildConnection.getConnection();
+            String SQLcmd = "select * from teacher where teacherId = ? AND password = ?";
+            PreparedStatement pst = conn.prepareStatement(SQLcmd);
+            pst.setLong(1, id);
+            pst.setString(2, password);
+            ResultSet rs = pst.executeQuery();           
+            if(rs.next()){
+                t.setName(rs.getString("name"));
+                t.setTeacherId(rs.getLong("teacherId"));
+            }else{
+                t = null;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return t;
+    }
     
 }
