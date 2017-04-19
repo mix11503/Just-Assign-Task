b16618be1f33a3dafc85acee86f1022b3b018fb5
lx0ala <fr33dolvl@gmail.com>
2017-04-19 17:35:15 +0700

    แอดมิน:สร้างรายวิชา

--- src/java/com/junior/jat/model/Admin.java
+++ src/java/com/junior/jat/model/Admin.java
@@ -1,7 +1,6 @@
 package com.junior.jat.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 public class Admin {
     public static void addUser(String id,String password,String name,String status){
         try{
@@ -18,6 +17,21 @@ public class Admin {
             pstm.setString(2, password);
             pstm.setString(3, name);
             pstm.executeUpdate();         
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+    public static void createSubject(String id, String subjectName, String teacherId){
+        try{
+            Connection conn = BuildConnection.getConnection();
+            String sql = "INSERT INTO `subject` (`subjectId`, `subjectName`, `teacherId`) VALUES (?, ?, ?);";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, id);
+            pstm.setString(2, subjectName);
+            pstm.setLong(3, Long.parseLong(teacherId));
+            pstm.executeUpdate();     
         }
         catch(Exception e){
             System.out.println(e);
