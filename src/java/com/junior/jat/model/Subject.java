db9e2b161c803b8366e0f0e263641424563cfec0
NATTAWUT PICHAIWATTNPHON <nata8ify@gmail.com>
2017-05-22 22:34:40 +0700

    Fix Unexpected Bug
    * Subject not be revealed for another student when someone already
    subscribed.

--- src/java/com/junior/jat/model/Subject.java
+++ src/java/com/junior/jat/model/Subject.java
@@ -10,6 +10,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +20,7 @@ import java.util.logging.Logger;
  * @author Kittisak
  */
 public class Subject {
+
     private String subjectId;
     private String subjectName;
     private long teacherId;
@@ -30,8 +33,6 @@ public class Subject {
     public void setState(String state) {
         this.state = state;
     }
-    
-    
 
     public String getSubjectId() {
         return subjectId;
@@ -56,35 +57,38 @@ public class Subject {
     public void setTeacherId(long teacherId) {
         this.teacherId = teacherId;
     }
-    
-    public static ArrayList getSubject(long teacherId){
+
+    public static ArrayList getSubject(long teacherId) {
         ArrayList subjects = new ArrayList();
-        Subject subject = new Subject(); 
-        try{
-        Connection conn = BuildConnection.getConnection();
-        String SQLcmd = "select * from subject where teacherId = ?";
-        PreparedStatement pst = conn.prepareStatement(SQLcmd);
-        pst.setLong(1, teacherId);
-        ResultSet rs = pst.executeQuery();
-        while(rs.next()){
-            subject = new Subject();
-            subject.setSubjectId(rs.getString("subjectId"));
-            subject.setSubjectName(rs.getString("subjectName"));
-            subject.setTeacherId(rs.getLong("teacherId"));
-            subjects.add(subject);
-        }
-        return subjects;
-       }catch(SQLException se){
+        Subject subject = new Subject();
+        try {
+            Connection conn = BuildConnection.getConnection();
+            String SQLcmd = "select * from subject where teacherId = ?";
+            PreparedStatement pst = conn.prepareStatement(SQLcmd);
+            pst.setLong(1, teacherId);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                subject = new Subject();
+                subject.setSubjectId(rs.getString("subjectId"));
+                subject.setSubjectName(rs.getString("subjectName"));
+                subject.setTeacherId(rs.getLong("teacherId"));
+                subjects.add(subject);
+            }
+            return subjects;
+        } catch (SQLException se) {
             System.out.println(se);
-       }
+        }
         return null;
     }
+
     @Override
     public String toString() {
-        return "Subject{" + "subjectId=" + subjectId + ", subjectName=" + subjectName + ", teacherId=" + teacherId + '}';
+        return "Subject{" + "subjectId=" + subjectId + ", subjectName=" + subjectName + ", teacherId=" + teacherId + ", state=" + state + '}';
     }
+
     
-    public static ArrayList getSubject(){
+
+    public static ArrayList getSubject() {
         ArrayList subjects = new ArrayList();
         Subject subject = new Subject();
         try {
@@ -92,62 +96,90 @@ public class Subject {
             String sqlCmd = "SELECT * FROM subject";
             PreparedStatement pstm = conn.prepareStatement(sqlCmd);
             ResultSet rs = pstm.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 subject = new Subject();
                 subject.setSubjectId(rs.getString("subjectId"));
                 subject.setSubjectName(rs.getString("subjectName"));
                 subject.setTeacherId(rs.getLong("teacherId"));
                 subjects.add(subject);
-            }            
+            }
         } catch (SQLException ex) {
             Logger.getLogger(Subject.class.getName()).log(Level.SEVERE, null, ex);
         }
         return subjects;
     }
-    
-    public static ArrayList getSearchSubject(String keyword){
+
+    public static ArrayList getSearchSubject(String keyword) {
         ArrayList subjects = new ArrayList();
         Subject subject = new Subject();
         try {
             Connection conn = BuildConnection.getConnection();
             String sqlCmd = "SELECT * FROM subject WHERE subjectName like ?";
             PreparedStatement pstm = conn.prepareStatement(sqlCmd);
-            pstm.setString(1, "%"+keyword+"%");
+            pstm.setString(1, "%" + keyword + "%");
             ResultSet rs = pstm.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 subject = new Subject();
                 subject.setSubjectId(rs.getString("subjectId"));
                 subject.setSubjectName(rs.getString("subjectName"));
                 subject.setTeacherId(rs.getLong("teacherId"));
                 subjects.add(subject);
-            }            
+            }
         } catch (SQLException ex) {
             Logger.getLogger(Subject.class.getName()).log(Level.SEVERE, null, ex);
         }
         return subjects;
     }
-    
-    public static ArrayList getSubjectWithState(long studentId){
+
+    public static ArrayList getSubjectWithState(long studentId) {
         ArrayList subjects = new ArrayList();
         Subject subject = new Subject();
         try {
             Connection conn = BuildConnection.getConnection();
-            String sqlCmd = "SELECT * FROM subject s left join map_st_subj mst on s.subjectId = mst.subjectId where mst.studentId = ? or mst.studentId IS NULL;";
+            String sqlCmd = "SELECT * FROM subject s;";
             PreparedStatement pstm = conn.prepareStatement(sqlCmd);
-            pstm.setLong(1,studentId);
             ResultSet rs = pstm.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 subject = new Subject();
                 subject.setSubjectId(rs.getString("subjectId"));
                 subject.setSubjectName(rs.getString("subjectName"));
                 subject.setTeacherId(rs.getLong("teacherId"));
-                subject.setState(rs.getString("approve_state"));
                 subjects.add(subject);
-            }            
+            }
+            approvalSetter(studentId, subjects);
+        pstm.close();
+        conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(Subject.class.getName()).log(Level.SEVERE, null, ex);
         }
         return subjects;
     }
-            
+
+    /** Set state for each Student Subject */
+    private static void approvalSetter(long studentId, ArrayList<Subject> subjects) throws SQLException {
+        Connection conn = BuildConnection.getConnection();
+        String sqlCmd;
+        PreparedStatement pstm = null;
+        ResultSet rs;
+        for (Subject s : subjects) {
+            sqlCmd = "SELECT approve_state FROM `map_st_subj` WHERE `studentId` = ? AND `subjectId` = ?;;";
+            if(pstm == null){
+            pstm = conn.prepareStatement(sqlCmd);
+            } else {
+               pstm.clearParameters();
+            }
+            pstm.setLong(1, studentId);
+            pstm.setString(2, s.getSubjectId());
+            rs = pstm.executeQuery();
+            if(rs.next()){
+                s.setState(rs.getString(1));
+            }
+        }
+        pstm.close();
+        conn.close();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSubjectWithState(59130500048L));
+    }
 }
